dcf5258982af319b2cad2540f703decd1b562c7c
Dharanish <126368315+DharanishShivkiranKarthikeyan@users.noreply.github.com>
2025-03-29 22:31:08 -0400

    niognp43twnwfpp

--- web/dht.js
+++ web/dht.js
@@ -40,7 +40,15 @@ export class DHT {
 
   // Utility to generate a random suffix for peerId
   generateRandomSuffix() {
-    return Math.random().toString(36).substring(2, 8); // Generates a 6-character random string
+    let length = 5;
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let result = "";
+    for (let i = 0; i < length; i++) {
+      result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return result;
+  
+    
   }
 
   async initializeKnownNodes() {
