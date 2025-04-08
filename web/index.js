0647dacbc1c48bf97906cdc720d6f1d37dd407ea
Dharanish <126368315+DharanishShivkiranKarthikeyan@users.noreply.github.com>
2025-04-08 10:33:01 -0400

    bububuobuob

--- web/index.js
+++ web/index.js
@@ -196,7 +196,7 @@ async function updateLiveFeed() {
     console.log(snippetsSnapshot)
     const snippetsData = {};
     snippetsSnapshot.forEach((doc) => {
-      try{console.log(doc.data)}
+      try{console.log(doc.data+"EGAJOOHQAGIQING")}
       catch{}
       snippetsData[doc.id] = doc.data();
     });
