5fe5fed5d9cce9f8285fa9e72e8f43a666529933
Dharanish <126368315+DharanishShivkiranKarthikeyan@users.noreply.github.com>
2025-05-01 14:11:10 -0400

    buo;buobunpbiphbphibhoubioyibboi

--- web/src/firebase.js
+++ web/src/firebase.js
@@ -1,6 +1,7 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js';
-import { getAuth } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
     apiKey: "AIzaSyBrdrwvY-lPObZgortEgw7YWycUOGsBlyM",
