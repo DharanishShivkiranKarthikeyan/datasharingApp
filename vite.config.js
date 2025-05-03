49fce507ef8c871ef8465ae6bfe63748f6402c20
Dharanish <126368315+DharanishShivkiranKarthikeyan@users.noreply.github.com>
2025-05-03 17:58:32 -0400

    rnrnrnrg

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { cpSync } from 'fs';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
@@ -9,16 +10,21 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       output: {
-        manualChunks: undefined,
-        entryFileNames: 'assets/app.js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]',
+        // Bundle all JavaScript into a single file
+        manualChunks: undefined, // Disable chunk splitting
+        entryFileNames: 'assets/app.js', // Name the single bundle file "app.js"
+        chunkFileNames: 'assets/[name].js', // Fallback for any chunks (shouldn't be used)
+        assetFileNames: 'assets/[name].[ext]', // For other assets like CSS
       },
     },
   },
-  base: '/datasharingApp/',
+  base: '/datasharingApp/', // Updated to match your repository name
   server: {
     open: '/index.html',
-    port: 3000
-  }
+    port: 3000,
+    https: {
+      key: './cert.key',
+      cert: './cert.pem'
+    }
+  },
 });
