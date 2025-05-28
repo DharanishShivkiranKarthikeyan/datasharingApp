59b669afe3c694baca748fb04b78c0e7cd9a1895
Dharanish <126368315+DharanishShivkiranKarthikeyan@users.noreply.github.com>
2025-05-27 21:24:39 -0400

    Updated package-lock.json

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,21 @@
 import { defineConfig } from 'vite';
-import { cpSync } from 'fs';
+import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
 export default defineConfig({
-  root: './web',
+  plugins: [react()],
+  root: './src',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
     rollupOptions: {
-      input:{
-        main: resolve("./web/","index.html"),
-        signup: resolve("./web/","signup.html"),
-        nodeinstructions: resolve("./web/","node-instructions.html")
-      },
-      output: {
-        // Bundle all JavaScript into a single file
-        manualChunks: undefined, // Disable chunk splitting
-        entryFileNames: 'assets/app.js', // Name the single bundle file "app.js"
-        chunkFileNames: 'assets/[name].js', // Fallback for any chunks (shouldn't be used)
-        assetFileNames: 'assets/[name].[ext]', // For other assets like CSS
-      },
-    },
+      input: {
+        main: resolve(__dirname, 'src/index.html')
+      }
+    }
   },
-  base: '/datasharingApp/', // Updated to match your repository name
+  base: '/datasharingApp/',
   server: {
-    open: '/index.html',
     port: 3000,
     https: {
       key: './cert.key',
@@ -33,24 +24,11 @@ export default defineConfig({
   },
   optimizeDeps: {
     include: [
-      'libp2p',
-      '@libp2p/kad-dht',
-      '@libp2p/peer-id-factory',
-      '@libp2p/identify',
-      '@libp2p/ping',
-      '@multiformats/multiaddr',
-      '@multiformats/cid',
       'peerjs',
       'events'
     ],
     esbuildOptions: {
       target: 'esnext'
-    }
-  },
-  resolve: {
-    alias: {
-      crypto: 'crypto-browserify',
-      events: 'events' // Alias Node.js events to events package
     }
   }
 });
